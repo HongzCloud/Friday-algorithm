75ed2a01aa090e8c08b391915deef1d91c5c4136
HongzCloud <jihong21511@gmail.com>
2020-11-06 15:38:27 +0900

    BackJoon p1008

---
+++ 1week-algorithm/1week-algorithm/p1008.swift
@@ -0,0 +1,15 @@
+//
+//  p1008.swift
+//  1week-algorithm
+//
+//  Created by 오킹 on 2020/11/06.
+//
+
+import Foundation
+
+func p1008() {
+    let a = Float(readLine()!)!
+    let b = Float(readLine()!)!
+    
+    print(a/b)
+}
