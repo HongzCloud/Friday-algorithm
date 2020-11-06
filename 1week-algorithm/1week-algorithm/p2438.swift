d8cc7d87ce72cdafba31bdbd83db5295fd79da27
HongzCloud <jihong21511@gmail.com>
2020-11-06 15:40:02 +0900

    BackJoon p2438

---
+++ 1week-algorithm/1week-algorithm/p2438.swift
@@ -0,0 +1,19 @@
+//
+//  p2438.swift
+//  1week-algorithm
+//
+//  Created by 오킹 on 2020/11/06.
+//
+
+import Foundation
+
+func p2438() {
+    let a  = Int(readLine()!)!
+    
+    for n in 0...a {
+        for _ in 0..<n {
+            print("*", separator: "", terminator: "")
+        }
+        print()
+    }
+}
