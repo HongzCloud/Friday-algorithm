7ed56d6c6372a758a51ee549f080aeb9c80338c9
HongzCloud <jihong21511@gmail.com>
2020-11-06 15:52:22 +0900

    BackJoon p11654

---
+++ 1week-algorithm/1week-algorithm/p11654.swift
@@ -0,0 +1,14 @@
+//
+//  p11654.swift
+//  1week-algorithm
+//
+//  Created by 오킹 on 2020/11/06.
+//
+
+import Foundation
+
+func p11654() {
+    let a: Character = Character(readLine()!)
+    
+    print((a.asciiValue)!)
+}
