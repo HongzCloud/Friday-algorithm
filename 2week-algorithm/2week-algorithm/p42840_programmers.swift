918618def19cd1b6da3b9563e7d31f9cec2ce5f7
HongzCloud <jihong21511@gmail.com>
2020-11-14 15:59:08 +0900

    programmers p42840

---
+++ 2week-algorithm/2week-algorithm/p42840_programmers.swift
@@ -0,0 +1,39 @@
+//
+//  p42840_programmers.swift
+//  2week-algorithm
+//
+//  Created by 오킹 on 2020/11/13.
+//
+
+import Foundation
+
+struct Student {
+    var patten: [Int]
+    var score: Int = 0
+}
+
+func p42840(_ answers:[Int]) -> [Int] {
+    var pattenCount: Int = 0
+    var students = [Student(patten: [1, 2, 3, 4, 5]),
+                       Student(patten: [2, 1, 2, 3, 2, 4, 2, 5]),
+                       Student(patten: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5])]
+ 
+    for studentNumbers in 0..<students.count {
+        for answersCount in 0..<answers.count {
+            if answers[answersCount] == students[studentNumbers].patten[pattenCount] {
+                students[studentNumbers].score += 1
+            }
+            pattenCount += 1
+            
+            if pattenCount == students[studentNumbers].patten.count {
+                pattenCount = 0
+            }
+        }
+        pattenCount = 0
+    }
+    
+    let maxScore = students.max(by: { $0.score < $1.score })?.score ?? 0
+    let scoreIndexPlusOne = students.indices.filter({students[$0].score == maxScore}).map({$0 + 1})
+
+    return scoreIndexPlusOne
+}
