304e0bb09b8ab31111844b258d0c952148fa9e9d
HongzCloud <jihong21511@gmail.com>
2020-11-20 14:51:27 +0900

    HackerRank BigSum

---
+++ 3week-algorithm/3week-algorithm/BigSum_HackerRank.swift
@@ -0,0 +1,12 @@
+//
+//  BigSum_HackerRank.swift
+//  3week-algorithm
+//
+//  Created by 오킹 on 2020/11/20.
+//
+
+import Foundation
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0 , +)
+}
--- 3week-algorithm/3week-algorithm/main.swift
+++ 3week-algorithm/3week-algorithm/main.swift
@@ -8,10 +8,22 @@
 import Foundation
 
 //MARK:- 1번문제
+//print("4번문제")
 //print(a + b)
+//print()
 
 //MARK:- 2번문제
+//print("4번문제")
 //print(gradingStudents(grades: [78, 32, 43, 67]))
+//print()
 
 //MARK:- 3번문제
-print(compareTriplets(a: [1,2,3], b: [3,2,1]))
+//print("4번문제")
+//print(compareTriplets(a: [1,2,3], b: [3,2,1]))
+//print()
+
+//MARK:- 4번문제
+print("4번문제")
+print(aVeryBigSum(ar: [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
+print()
+
