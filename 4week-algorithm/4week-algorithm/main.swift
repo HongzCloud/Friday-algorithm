a3f251c4165ee9ad1edfae80a89ee03ae4a8a13a
HongzCloud <jihong21511@gmail.com>
2020-11-27 15:45:46 +0900

    LeetCode ReverseInt

--- 4week-algorithm/4week-algorithm/main.swift
+++ 4week-algorithm/4week-algorithm/main.swift
@@ -13,4 +13,7 @@ print(twoSumTest.twoSum([0,4,3,0], 0))
 print(twoSumTest.twoSum([-1,-2,-3,-4,-5], -8))
 
 //MARK:- 2번문제
+let reverseManager = Solution()
+print(reverseManager.reverse(-120))
+print(reverseManager.reverse(1534236469))
 
